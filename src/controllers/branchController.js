7dd12883b662d3b2e23b86f45fdd297da989700f
rameshkasula <rameshlearningadvance@gmail.com>
2023-11-16 23:19:19 +0530

    branches pagination

--- src/controllers/branchController.js
+++ src/controllers/branchController.js
@@ -5,7 +5,14 @@ import branchModel from "../models/branchModel.js";
 
 export const getBranches = async (req, res) => {
   try {
-    const branches = await branchModel.find({});
+    // with pagination
+    const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
+    const pageSize = parseInt(req.query.pageSize) || 10; // Default to 10 items per page
+
+    // Calculate the number of documents to skip based on the page and pageSize
+    const skip = (page - 1) * pageSize;
+
+    const branches = await branchModel.find().skip(skip).limit(pageSize);
     console.log("length", branches.length);
     return res
       .status(HttpStatus.ALREADY_EXISTS.code)
@@ -18,6 +25,15 @@ export const getBranches = async (req, res) => {
         )
       );
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    return res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR.code)
+      .json(
+        new Response(
+          HttpStatus.INTERNAL_SERVER_ERROR.code,
+          HttpStatus.INTERNAL_SERVER_ERROR.status,
+          HttpStatus.INTERNAL_SERVER_ERROR.message,
+          error
+        )
+      );
   }
 };
