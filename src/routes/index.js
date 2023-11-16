535f82b93dd83a11586c02cd817e3acfe1e424bf
rameshkasula <rameshlearningadvance@gmail.com>
2023-11-16 23:51:44 +0530

    chanages

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,12 +11,4 @@ appRouter.use("/branch", branchRouter);
 appRouter.use("/orders", orderRouter);
 appRouter.use("/items", bakeryItemRouter);
 
-// no route
-appRouter.use("*", (req, res) => {
-  res.status(404).json({
-    status: 404,
-    message: "Route not found",
-  });
-});
-
 export default appRouter;
