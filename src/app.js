1326f4a1ed9c7dc32f3ec7126b26b4c80d447a75
rameshkasula <rameshlearningadvance@gmail.com>
2023-11-16 23:34:05 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -13,7 +13,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 // function to insert data
